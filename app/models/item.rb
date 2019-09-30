0052521f26fc5e01af975c605f19b25141d55c73
James Sharpe <jameswrightsharpe@gmail.com>
2019-09-30 11:48:17 -0400

    fixed methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   def self.create_deck(name, brand, price_in_cents, description, image_url, length, width, material, mounting_type)
     create(
-      create_default(name, brand, price_in_cents, description, image_url),
+      name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url,
       type: "deck",
       deck_length: length,
       deck_width: width,
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   def self.create_trucks(name, brand, price_in_cents, description, image_url, color, size_in_mm, standard_kingpin, axel_degree)
     create(
-      create_default(name, brand, price_in_cents, description, image_url),
+      name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url,
       type: "trucks",
       color: color,
       trucks_size_in_mm: size_in_mm,
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
 
   def self.create_wheels(name, brand, price_in_cents, description, image_url, color, durometer, size_in_mm, center_cut)
     create(
-      create_default(name, brand, price_in_cents, description, image_url),
+      name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url,
       type: "wheels",
       color: color,
       wheels_size_in_mm: size_in_mm,
@@ -34,7 +34,7 @@ class Item < ApplicationRecord
 
   def self.create_bearings(name, brand, price_in_cents, description, image_url, open, abec)
     create(
-      create_default(name, brand, price_in_cents, description, image_url),
+      name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url,
       type: "bearings",
       bearings_open?: open,
       bearings_abec?: abec,
@@ -43,16 +43,10 @@ class Item < ApplicationRecord
 
   def self.create_grip_tape(name, brand, price_in_cents, description, image_url, color, length, width)
     create(
-      create_default(name, brand, price_in_cents, description, image_url),
+      name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url,
       type: "grip_tape",
       grip_tape_length: length,
       grip_tape_width: width,
     )
-  end
-
-  private
-
-  def self.create_default(name, brand, price_in_cents, description, image_url)
-    { name: name, brand: brand, price_in_cents: price_in_cents, description: description, image_url: image_url }
   end
 end
